ce595327b0a7ab2d08baac66a5b6fe64f6e2bb8f
Alexandr20-ur <84001221+Alexandr20-ur@users.noreply.github.com>
2021-08-17 14:17:30 +0400

    калькулятор

---
+++ result.php
@@ -0,0 +1,15 @@
+<?php
+
+$result = require __DIR__ . '/calc.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+   <title>результат вычисления</title>
+</head>
+<body>
+    <?= $result ?>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Калькулятор</title>
+</head>
+<body>
+    <form action="/result.php" method="get">
+    <input type="text" name="x1">
+    <select name="operation">
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value='/'>/</option>
+        <option value='*'>*</option>
+    <input type="text" name="x2">
+    <input type="submit" value="Посчитать"> 
+</body>
+</html>
