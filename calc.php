287e56b0e0a182174f731d6dd5e7c2b169a189c4
Alexandr20-ur <84001221+Alexandr20-ur@users.noreply.github.com>
2021-08-28 10:46:05 +0400

    Update calc.php

--- calc.php
+++ calc.php
@@ -8,8 +8,12 @@ if (empty($_GET['operation'])) {
     return 'Операция не передана';
 }
 
-if (empty($_GET['x1']) || empty($_GET['x2'])) {
+if (!isset($_GET['x1']) || !isset($_GET['x2'])) {
     return 'Аргументы 1 или 2 не переданы';
+} 
+
+if (!is_numeric($x1) || !is_numeric($x2)) {
+    return 'Аргументы должны быть числами';
 }
 
 $x1 = $_GET['x1'];
@@ -26,10 +30,14 @@ switch ($operation) {
     case '-':
         $result = $x1 - $x2;
         break;
-    case '/';
+    case '/':
+        if ($x2 < 1) {
+            return 'На ноль делить нельзя';
+        } else {
         $result = $x1 / $x2;
         break;
-    case '*';
+        }
+    case '*':
         $result = $x1 * $x2;
         break;
 }
